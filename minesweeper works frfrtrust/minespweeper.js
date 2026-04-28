55b2a99b0faccba6c6e34301bb1e1752a5f98b2d
imris <imri.sutton.ps@gmail.com>
2026-04-28 14:22:13 +0300

    lowkey just made ts work

--- minesweeper works frfrtrust/minespweeper.js
+++ minesweeper works frfrtrust/minespweeper.js
@@ -1,48 +1,61 @@
 ﻿var grid = [];
-var isMine
-var isRevealed
-var isFlagged
-var neighborCount
-var td
-for (var row = 0; row < 11; row++) {
-    grid[row] = [];
-    for (var collum = 0; collum < 11; collum++) {
-        grid[row][collum] = [
-            isMine = false,
-            isRevealed= false,
-            isFlagged= false,
-            neighborCount = 0,
-            td = document.createElement("td"),
-            ]
+var rows = parseInt(prompt("how many rows?"));
+var collum = parseInt(prompt("how many collums?"));
+function start() {
+    var tables = document.getElementById("minetable");
+    for (let i = 0; i < rows; i++) {
+        grid[i] = [];
+        var tr = document.createElement("tr");
+           
+        for (let j = 0; j < collum; j++) {
+            var td = document.createElement("td");
+            var img = document.createElement("img");
+            img.src = "masked_tile.png";
+            img.onclick = function () {
+                reveal(i, j);
+            };
 
-    }
-}
-function laymines() {
-    var totmines = parseInt(alert("input number"));
-    var mineplaced = 0;
-    var n1 = Math.floor(Math.random() * 11)
-    var n2 = Math.floor(Math.random() * 11)
-    while (totmines >= mineplaced) {
-        if (grid[n1][n2].isMine == false) {
-            grid[n1][n2].isMine = true;
-            mineplaced++;
+            td.appendChild(img);
+            tr.appendChild(td);
+
+            grid[i][j] = {
+                isMine: false,
+                isRevealed: false,
+                isFlagged: false,
+                neighborCount: 0,
+                td: td
+            };
         }
 
+        tables.appendChild(tr);
     }
+    laymines();
+    neighbor();
 }
-function neigbor() {
-    for (var row = 0; row < 11; row++) {
-        for (var collum = 0; collum < 11; collum++) {
-            if (grid[row][collum].isMine == false) {
-                for (var j = -1; j <= 1; j++) {
-                    for (var i = -1; i <= 1; i++) {
-                        if (grid[row + j] != undefined && grid[row + j][collum + i] != undefined) {
 
+    function laymines() {
+        var totmines = parseInt(prompt("how many mines?"));
+        var mineplaced = 0;
+        while (mineplaced < totmines) {
+            var n1 = Math.floor(Math.random() * rows);
+            var n2 = Math.floor(Math.random() * collum);
+            if (grid[n1][n2].isMine == false) {
+                grid[n1][n2].isMine = true;
+                mineplaced++;
+            }
+        }
+    }
 
-                            if (grid[row + j][collum + i].isMine) {
-                                grid[row][collum].neighborCount++;
+function neighbor() {
+    for (let o = 0; o < rows; o++) {
+        for (let p = 0; p < collum; p++) {
+            if (grid[o][p].isMine == false) {
+                for (let j = -1; j <= 1; j++) {
+                    for (let i = -1; i <= 1; i++) {
+                        if (grid[o + j] != undefined && grid[o + j][p + i] != undefined) {
+                            if (grid[o + j][p + i].isMine) {
+                                grid[o][p].neighborCount++;
                             }
-
                         }
                     }
                 }
@@ -52,13 +65,21 @@ function neigbor() {
 }
 
 function reveal(row, collum) {
+    if (grid[row][collum] === undefined) {
+        return;
+    }
     var cell = grid[row][collum];
-    var img = document.createElement("img");
+    if (cell.isRevealed || cell.isFlagged) return;
+    cell.isRevealed = true;
 
+    var img = document.createElement("img");
     if (cell.isMine) {
         img.src = "revealed_tile_bomb.png";
+        lostgame();
+
     } else if (cell.neighborCount == 0) {
-        img.src = "revealed_tile_0.png";
+        img.src = "revealed_tile.png";
+        revealempty();
     } else if (cell.neighborCount == 1) {
         img.src = "revealed_tile_1.png";
     } else if (cell.neighborCount == 2) {
@@ -77,5 +98,31 @@ function reveal(row, collum) {
         img.src = "revealed_tile_8.png";
     }
 
+    cell.td.innerHTML = "";
     cell.td.appendChild(img);
 }
+function lostgame() {
+    alert("you lost")
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < collum; j++) {
+         var cell = grid[i][j];
+            if (cell.isMine) {
+                var mineImg = document.createElement("img");
+                mineImg.src = "revealed_tile_bomb.png";
+                cell.td.innerHTML = "";
+                cell.td.appendChild(mineImg);
+            }
+        }
+    }
+    location.reload();
+
+}
+function revealempty() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < collum; j++) {
+            
+        }
+    }
+
+}
+start();
